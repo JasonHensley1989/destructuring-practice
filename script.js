808d559f92c2e56f785d2f46b6e65238aa3647de
Jason Hensley <jasonhensley1989@gmail.com>
2023-02-12 12:03:17 -0600

    nested destructing finished

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const user = {
         1: "Learn Destructuring",
         2: "Spread Operators",
         3: "Constructors",
-        4: "Getters and Setters"
+        4: "Getters and Setters",
     }
 }
 
@@ -16,5 +16,12 @@ const user = {
 //     todos:{a, b, c, d}
 // } = user;
 
-let { firstName: name, country: location1, todos,  todos: { a, b, c, d }} = user;
-console.log(location1, a);
+let { 
+    firstName: name, 
+    country: location1, 
+    todos, 
+    todos: { 1:a, 2:b, 3:c, 4:d },
+} = user;
+console.log(a, b, c, d);
+
+// REMEMBER TO PUT REASSIGNMENTS WITH THE ORIGINAL FOLLOWED BY THE COLON AND NEW NAME
