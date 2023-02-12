260958e899a10ab42e9b52bb64cad085975b4547
Jason Hensley <jasonhensley1989@gmail.com>
2023-02-12 12:18:35 -0600

    array destructuring, skipping values

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const user = {
-    firstName: "Jason",
-    country: "United States",
-    todos: {
-        1: "Learn Destructuring",
-        2: "Spread Operators",
-        3: "Constructors",
-        4: "Getters and Setters",
-    }
-}
+// const user = {
+//     firstName: "Jason",
+//     country: "United States",
+//     todos: {
+//         1: "Learn Destructuring",
+//         2: "Spread Operators",
+//         3: "Constructors",
+//         4: "Getters and Setters",
+//     }
+// }
 
 // let { 
 //     firstname, 
@@ -16,12 +16,20 @@ const user = {
 //     todos:{a, b, c, d}
 // } = user;
 
-let { 
-    firstName: name, 
-    country: location1, 
-    todos, 
-    todos: { 1:a, 2:b, 3:c, 4:d },
-} = user;
-console.log(a, b, c, d);
+// let { 
+//     firstName: name, 
+//     country: location1, 
+//     todos, 
+//     todos: { 1:a, 2:b, 3:c, 4:d },
+// } = user;
+// console.log(a, b, c, d);
 
 // REMEMBER TO PUT REASSIGNMENTS WITH THE ORIGINAL FOLLOWED BY THE COLON AND NEW NAME
+// Create Array with list of values and use destructuring to destructure the values
+// Add a nested array with any 5 values, to the destructuring while skipping second value, and forth
+const learningList = ["recursion", "nickelodeon", "closures", "hulu", "Elohim"];
+
+let [ a,,b,,c] = learningList;
+
+console.log(a, b, c);
+
